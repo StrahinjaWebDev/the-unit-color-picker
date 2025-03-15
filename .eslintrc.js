ebe8ac43fffa601754aca0018205647ec3eca0b8
Strahinja <strahinjdevloper@gmail.com>
2025-03-15 13:53:34 +0100

    SV feat(setup): initial project setup

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react/recommended',
+        'plugin:jsx-a11y/recommended',
+        'plugin:tailwind/recommended',
+        'prettier',
+    ],
+    plugins: ['@typescript-eslint', 'react', 'jsx-a11y', 'tailwind'],
+    rules: {
+    },
+}; 
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="p-4 border rounded-lg">
+        <p className="text-amber-950 font-bold">dsa</p>
+      </div>
+    </>
+  )
+}
+
+export default App
