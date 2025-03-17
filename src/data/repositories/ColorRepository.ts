78ab1bf1c33aabfaedcc6a65987282100acf2134
Strahinja <strahinjdevloper@gmail.com>
2025-03-17 01:23:59 +0100

    SV fix(run): adding test running method when starting the app

--- src/data/repositories/ColorRepository.ts
+++ src/data/repositories/ColorRepository.ts
@@ -1,10 +1,9 @@
 import axios from 'axios';
 import { Color } from '../../core/entities/Color.ts';
 import { IColorRepository } from '../../core/interfaces/IColorRepository';
-import { ENV } from '../../core/config/env.ts';
 
 export class ColorRepository implements IColorRepository {
-  private mockApiColorsUrl = `${ENV.VITE_MOCK_API_URL}/colors`;
+  private mockApiColorsUrl = `https://67d58c4e286fdac89bbfa7ca.mockapi.io/colors`;
 
   async getColors(): Promise<Color[]> {
     const response = await axios.get<Color[]>(this.mockApiColorsUrl);
